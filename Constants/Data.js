611d8fc6d25773888e9a3955123ecb47806ac90d
vsifiwe <asifiwemanzi@gmail.com>
2020-10-03 11:18:04 +0200

    adding data template

---
+++ Constants/Data.js
@@ -0,0 +1,12 @@
+const info = [
+    {
+        screen = 'Videos',
+        cards =[
+            {
+                title: 'Arctic Greening Driven By Warmer Temperatures',
+                link: 'https://youtu.be/htzq2QbTKtw',
+                picture: '../assets/images/1.png'
+            }
+        ]
+    }
+]
