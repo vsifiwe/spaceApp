245c5016325d8070f25a3d1dd9e656c595ad4b04
vsifiwe <asifiwemanzi@gmail.com>
2020-10-04 20:46:47 +0200

    Final additions

--- Constants/Images.js
+++ Constants/Images.js
@@ -29,6 +29,63 @@ const images = [
             'https://eoimages.gsfc.nasa.gov/images/imagerecords/78000/78314/VIIRS_3Feb2012_tn.jpg',
         text: 'North Africa and Europe from Suomi NPP',
     },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/78000/78314/VIIRS_3Feb2012_tn.jpg',
+        text: 'North Africa and Europe from Suomi NPP',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/78000/78314/VIIRS_3Feb2012_tn.jpg',
+        text: 'North Africa and Europe from Suomi NPP',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/8000/8108/ipcc_bluemarble_west_front.jpg',
+        text: 'Twin Blue Marbles',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73934/srtm_ramp2.worldx294x196.jpg',
+        text: 'Topography',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73963/gebco_08_rev_bath_294x196_color.jpg',
+        text: 'Bathymetry',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73909/world.topo.bathy.200412x294x196.jpg',
+        text:
+            'December, Blue Marble Next Generation w/ Topography and Bathymetry',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74218/world.200412x294x196.jpg',
+        text: 'December, Blue Marble Next Generation',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74518/world.topo.200412x294x196.jpg',
+        text: 'December, Blue Marble Next Generation w/ Topography',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73884/world.topo.bathy.200411x294x196.jpg',
+        text:
+            'November, Blue Marble Next Generation w/ Topography and Bathymetry',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74192/world.200411x294x196.jpg',
+        text: 'November, Blue Marble Next Generation',
+    },
+    {
+        link:
+            'https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74493/world.topo.200411x294x196.jpg',
+        text: 'November, Blue Marble Next Generation w/ Topography',
+    },
 ];
 
 export default images;
